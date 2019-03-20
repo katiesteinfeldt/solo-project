5241bc81e74ac00c6dd606e8bc66466dce1f220b
Katie Mangan <katie.mangan1@gmail.com>
2019-03-20 17:02:50 -0500

    create getMyParks function

--- src/components/MyParks/MyParks.js
+++ src/components/MyParks/MyParks.js
@@ -1,26 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-//import LogOutButton from '../LogOutButton/LogOutButton';
+// LogOutButton from '../LogOutButton/LogOutButton';
 
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
 
 class MyParks extends Component {
+  
+  componentDidMount = () => {
+    this.getMyParks();
+  }
+  
+  getMyParks = () => {
+    console.log('getting parks');
+  }
+
+
   render() {
     return (
-  <div>
-    
-    {/* <h1 id="welcome">
-      Welcome, { props.user.username }!
-    </h1>
-    <p>Your ID is: {props.user.id}</p> */}
-    {/* <LogOutButton className="log-in" /> */}
-    <h1>My Parks</h1>
-  </div>
+      <div>
+        <h1 id="welcome">
+          My Parks
+      </h1>
+        {/* <LogOutButton className="log-in" /> */}
+      </div>
     )
   }
-  }
+}
 
 // Instead of taking everything from state, we just want the user info.
 // if you wanted you could write this code like this:
