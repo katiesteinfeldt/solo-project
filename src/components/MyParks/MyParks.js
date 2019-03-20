68988489de9620c944275c89b72f171876eaa65d
Katie Mangan <katie.mangan1@gmail.com>
2019-03-20 16:58:31 -0500

    update MyParks to be class component

--- src/components/MyParks/MyParks.js
+++ src/components/MyParks/MyParks.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
+//import LogOutButton from '../LogOutButton/LogOutButton';
 
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
-const MyParks = (props) => (
+
+class MyParks extends Component {
+  render() {
+    return (
   <div>
-    <h1 id="welcome">
+    
+    {/* <h1 id="welcome">
       Welcome, { props.user.username }!
     </h1>
-    <p>Your ID is: {props.user.id}</p>
-    <LogOutButton className="log-in" />
+    <p>Your ID is: {props.user.id}</p> */}
+    {/* <LogOutButton className="log-in" /> */}
+    <h1>My Parks</h1>
   </div>
-);
+    )
+  }
+  }
 
 // Instead of taking everything from state, we just want the user info.
 // if you wanted you could write this code like this:
