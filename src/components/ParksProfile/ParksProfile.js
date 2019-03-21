432629d0e7da0159aba69020285250fdca190bff
Katie Mangan <katie.mangan1@gmail.com>
2019-03-21 14:11:39 -0500

    display parks on my profile

--- src/components/ParksProfile/ParksProfile.js
+++ src/components/ParksProfile/ParksProfile.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 class ParksProfile extends Component {
+  state = {
+    displayMyParks: false,
+  }
 
   componentDidMount = () => {
     this.getMyParks();
@@ -9,13 +13,43 @@ class ParksProfile extends Component {
 
   getMyParks = () => {
     console.log('going to get my parks from database');
+    axios.get('/myparks')
+      .then((response) => {
+        this.props.dispatch({ type: 'SET_MY_PARKS', payload: response.data })
+      }).catch(error => {
+        console.log('error in my parks client get request', error);
+      });
   }
 
+  displayParkInfo = (parks_visited_id) => {
+    return () => {
+      this.props.dispatch({ type: 'FETCH_CURRENT_PARK', payload: parks_visited_id })
+      this.setState({
+        displayParkInfo: true,
+        currentPark: parks_visited_id,
+      })
+    }
+  }
 
+  createMyParks =  () => {
+    return this.props.parks.map(park =>
+      <div key={park.all_parks_id}><h3>{park.park_full_name}</h3>
+        <img onClick={this.displayParkInfo(park.all_parks_id)} alt={park.park_description} src={park.image_path_1} />
+        <pre></pre>
+        Date Visited: {park.date_visited_1}
+        <pre></pre>
+        Notes: {park.notes}
+        <pre></pre>
+      </div>
+    )
+  }
+
+ 
   render() {
     return (
       <div>
         Parks PRofile lol
+        {this.createMyParks()}
       </div>
     );
   }
@@ -24,9 +58,12 @@ class ParksProfile extends Component {
 
 
 
-const mapStateToProps = reduxState => ({
-  reduxState,
+const mapStateToProps = state => ({
+  user: state.user,
+  parks: state.parks,
+  parkdisplay: state.parkdisplay,
 });
 
+// this allows us to use <App /> in index.js
 export default connect(mapStateToProps)(ParksProfile);
 
