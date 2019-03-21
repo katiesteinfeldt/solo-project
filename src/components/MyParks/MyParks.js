f7314916f8380a24457954cfc8df754cd8b87a9f
Katie Mangan <katie.mangan1@gmail.com>
2019-03-21 15:23:33 -0500

    display my parks on material ui cards

--- src/components/MyParks/MyParks.js
+++ src/components/MyParks/MyParks.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import './MyParks.css';
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import { CardContent, CardActions, Divider } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
 // LogOutButton from '../LogOutButton/LogOutButton';
 
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
+const styles = {
+  card: {
+    minWidth: 275,
+    maxWidth: 350,
+    margin: 22,
+  }
+}
 
 class MyParks extends Component {
   state = {
@@ -58,16 +70,21 @@ class MyParks extends Component {
 
   createMyParks = () => {
     return this.props.parks.map(park =>
-      <div key={park.all_parks_id}><h3>{park.park_full_name}</h3>
+      <Card className={this.props.classes.card} key={park.all_parks_id}><h3>{park.park_full_name}</h3>
+        <Divider />
+        <CardContent>
         <img onClick={this.displayParkInfo(park.all_parks_id)} alt={park.park_description} src={park.image_path_1} />
         <pre></pre>
         Date Visited: {park.date_visited_1}
         <pre></pre>
         Notes: {park.notes}
+        </CardContent>
+        <CardActions>
         {/* <button>Update</button> */}
         <button onClick={this.deletePark(park.parks_visited_id)}>Delete</button>
         <pre></pre>
-      </div>
+        </CardActions>
+      </Card>
     )
   }
 
@@ -99,7 +116,7 @@ closeParkDisplay = () => {
       currentParkDisplay = null;
     }
     return (
-      <div>
+      <Grid>
         <h1 id="welcome">
           My Parks
       </h1>
@@ -108,7 +125,7 @@ closeParkDisplay = () => {
           {this.createMyParks()}
         </div>
         {currentParkDisplay}
-      </div>
+      </Grid>
     )
   }
 }
@@ -123,4 +140,4 @@ const mapStateToProps = state => ({
 });
 
 // this allows us to use <App /> in index.js
-export default connect(mapStateToProps)(MyParks);
+export default withStyles(styles)(connect(mapStateToProps)(MyParks));
