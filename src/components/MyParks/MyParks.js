6ec6a8a9433a8d561c8b4f33135673c354366549
Katie Mangan <katie.mangan1@gmail.com>
2019-03-21 19:16:44 -0500

    remove random placement function from myparks component

--- src/components/MyParks/MyParks.js
+++ src/components/MyParks/MyParks.js
@@ -35,13 +35,9 @@ const styles = theme => ({
   },
 });
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
 function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
+  const top = 50;
+  const left = 50;
 
   return {
     top: `${top}%`,
