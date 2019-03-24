2e460c87825e34cc4174826a526c0b22c6703c77
Katie Mangan <katie.mangan1@gmail.com>
2019-03-24 11:29:54 -0500

    add code comments to my parks component and remove excess code

--- src/components/MyParks/MyParks.js
+++ src/components/MyParks/MyParks.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import './MyParks.css';
+
+// -- Material UI components -- //
 import Card from '@material-ui/core/Card';
 import { CardContent, Divider } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
@@ -9,13 +11,9 @@ import Modal from '@material-ui/core/Modal';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
-// LogOutButton from '../LogOutButton/LogOutButton';
-
-// this could also be written with destructuring parameters as:
-// const UserPage = ({ user }) => (
-// and then instead of `props.user.username` you could use `user.username`
 let editFormDisplay;
 
+// -- styling for Material UI components -- //
 const styles = theme => ({
   paper: {
     position: 'absolute',
@@ -44,6 +42,7 @@ const styles = theme => ({
   }
 });
 
+// -- styling for Material UI modal -- //
 function getModalStyle() {
   const top = 50;
   const left = 50;
@@ -71,6 +70,8 @@ class MyParks extends Component {
     this.getMyParks();
   }
 
+  // retrieves list of parks visited from database and saves the information
+  // in a reducer 
   getMyParks = () => {
     axios.get('/myparks')
       .then((response) => {
@@ -80,6 +81,8 @@ class MyParks extends Component {
       });
   }
 
+  // determines which park was clicked and retrieves additional information
+  // from the database; opens modal
   displayParkInfo = (parks_visited_id) => {
     return () => {
       this.props.dispatch({ type: 'FETCH_CURRENT_PARK', payload: parks_visited_id })
@@ -98,8 +101,7 @@ class MyParks extends Component {
     });
   }
 
-
-  //this will delete park from the database 
+  //this will delete a park visit from the parks_visited table in the database 
   deletePark = (parks_visited_id) => {
     return () => {
       const r = window.confirm("Are you sure you want to delete this park?");
@@ -117,26 +119,7 @@ class MyParks extends Component {
     }
   }
 
-  // editVisit = (parks_visited_id) => {
-  //   return () => {
-  //     //this.props.dispatch({ type: 'EDIT_PARK', payload: parks_visited_id })
-  //     console.log('edit visit has been clicked', this.state);
-  //     this.setState({
-  //       isInEditMode: true,
-  //     })
-  // axios({
-  //   method: 'PUT',
-  //   url: '/myparks/' + parks_visited_id,
-  //   // data: {
-  //   //   date_visited_1: 
-  //   //   notes: 
-  //   // }
-  // }).then((response) => {
-  //   console.log('park updated');
-  // })
-  //   }
-  // }
-
+  // displays list of parks visited on the dom in a material ui card
   createMyParks = () => {
     return this.props.parks.map(park =>
       <Card onClick={this.displayParkInfo(park.all_parks_id)} className={this.props.classes.card} key={park.all_parks_id}><Typography className={this.props.classes.cardTitle} variant="h4">{park.park_full_name}</Typography>
@@ -157,16 +140,17 @@ class MyParks extends Component {
     )
   }
 
+  //closes modal on click of "ok" button or when clicking on the background page
   closeParkDisplay = () => {
-    console.log('closing park display');
     this.setState({
       open: false,
     })
-    this.props.dispatch({ type: 'FETCH_CURRENT_PARK', payload: '' })
   }
 
+  //when save is clicked, the information that the user has typed into the input boxes
+  //gets sent to the database and the parks_visited table for that park is updated.
+  //the information is refreshed by calling the getMyParks function and is displayed on the DOM
   saveEditedInfo = () => {
-    console.log(this.state);
     this.setState({
       isInEditMode: false,
       dateVisited: this.state.dateVisited,
@@ -180,30 +164,20 @@ class MyParks extends Component {
         date_visited_1: this.state.dateVisited,
         notes: this.state.notes,
       }
-    }).then((response) => {
+    }).then(() => {
       this.getMyParks();
     })
-    //this.props.dispatch({type: 'EDIT_PARK', payload: this.state })
   }
 
-  cancelEditedInfo = () => {
-    console.log('cancel clicked');
-    this.setState({
-      isInEditMode: false,
-    })
-  }
-
-  // submitEditedInfo = () => {
-  //   console.log(this.state);
-  // }
-
+  //this will toggle the park input information between text and input boxes with 
+  //a save/cancel button
   changeEditMode = () => {
-    console.log('HI');
     this.setState({
       isInEditMode: !this.state.isInEditMode,
     })
   }
 
+  //toggles park visited information on the modal between text and input fields for the user to update 
   displayEditFormDisplay = () => {
     if (this.state.isInEditMode) {
       editFormDisplay =
@@ -219,7 +193,7 @@ class MyParks extends Component {
           </div>
           <pre></pre>
           <Button onClick={this.saveEditedInfo}>Save</Button>
-          <Button onClick={this.cancelEditedInfo}>Cancel</Button>
+          <Button onClick={this.changeEditMode}>Cancel</Button>
         </div>
     }
     else {
@@ -254,7 +228,6 @@ class MyParks extends Component {
               {editFormDisplay}
               <img alt={this.props.parkdisplay[0].park_description} src={this.props.parkdisplay[0].image_path_1} />
               <Button variant="contained" color="default" className={this.props.classes.button} onClick={this.closeParkDisplay}>OK</Button>
-              {/* <Button variant="contained" color="default" className={this.props.classes.button} onClick={this.editVisit(this.props.parkdisplay[0].id)}>Edit</Button> */}
               <Button variant="contained" color="default" className={this.props.classes.button} onClick={this.deletePark(this.props.parkdisplay[0].id)}>Delete This Visit</Button>
             </div>
           </Modal>
@@ -275,15 +248,11 @@ class MyParks extends Component {
   }
 }
 
-// Instead of taking everything from state, we just want the user info.
-// if you wanted you could write this code like this:
-// const mapStateToProps = ({user}) => ({ user });
 const mapStateToProps = state => ({
-  user: state.user,
+  //user: state.user,
   parks: state.parks,
   parkdisplay: state.parkdisplay,
   editpark: state.editpark,
 });
 
-// this allows us to use <App /> in index.js
 export default withStyles(styles)(connect(mapStateToProps)(MyParks));
