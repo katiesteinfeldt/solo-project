8f2189bc1e18f27329a9af4e4c545d30a4266d67
Katie Mangan <katie.mangan1@gmail.com>
2019-03-26 15:41:01 -0500

    create component to map current list of visited parks

--- server/routes/my.parks.router.js
+++ server/routes/my.parks.router.js
@@ -5,7 +5,9 @@ const axios = require('axios');
 
 router.get('/', (req, res) => {
     console.log('at server - my parks');
-    pool.query(`SELECT "parks_visited"."id" AS "parks_visited_id", "all_parks"."id" AS "all_parks_id", "park_full_name", "date_visited_1", "date_visited_2", "date_visited_3", "notes", "image_path_1", "park_description" FROM "parks_visited" JOIN "all_parks" ON "all_parks"."id"="parks_visited"."park_id";`)
+    pool.query(`SELECT "parks_visited"."id" AS "parks_visited_id", "all_parks"."id" AS "all_parks_id", "park_full_name", "date_visited_1", 
+    "date_visited_2", "date_visited_3", "latLong", "notes", "image_path_1", "park_description" FROM "parks_visited" 
+    JOIN "all_parks" ON "all_parks"."id"="parks_visited"."park_id";`)
         .then((result) => {
             res.send(result.rows);
         }).catch((error) => {
--- src/components/GoogleMaps/GoogleMaps.js
+++ src/components/GoogleMaps/GoogleMaps.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import { GoogleApiWrapper, Map } from "google-maps-react";
+import axios from 'axios';
+import { connect } from 'react-redux';
+import GoogleMapsInfo from './GoogleMapsInfo';
+
+
 
 class GoogleMaps extends Component {
 
@@ -11,8 +16,21 @@ class GoogleMaps extends Component {
         },
         loading: true
     };
+    
+    componentDidMount = () => {
+        this.getMyParks();
+    }
 
-    componentDidMount() {
+    getMyParks = () => {
+        axios.get('/myparks')
+            .then((response) => {
+                this.props.dispatch({ type: 'SET_MY_PARKS', payload: response.data })
+            }).catch(error => {
+                console.log('error in my parks client get request', error);
+            });
+    }
+
+    componentDidUpdate = () => {
         navigator.geolocation.getCurrentPosition(
             position => {
                 const { latitude, longitude } = position.coords;
@@ -40,7 +58,13 @@ class GoogleMaps extends Component {
         );
     }
 
+    // for each park in the reducer => park
+        // const marker = new window.google.maps.Marker({
+        //     position: {lat: park.latitude, long: park.longitude}
+        // })
+
     render() {
+      
         const { loading, userLocation } = this.state;
         const { google } = this.props;
 
@@ -48,11 +72,26 @@ class GoogleMaps extends Component {
             return null;
         }
 
-        return <Map google={google} initialCenter={userLocation} zoom={4} />;
+       
+
+        return (
+            <div>
+        <Map google={google} initialCenter={userLocation} zoom={4} />
+        <GoogleMapsInfo />
+            </div>
+        );
 
     }
 }
+const mapStateToProps = state => ({
+    //user: state.user,
+    parks: state.parks,
+    parkdisplay: state.parkdisplay,
+    editpark: state.editpark,
+});
 
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyCHYi2wtrFyckBkYUz6Brsx7mB2z5khGHM'
-})(GoogleMaps);
+})(connect(mapStateToProps)(GoogleMaps));
+
+
