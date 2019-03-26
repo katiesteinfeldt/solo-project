0af7952da03dbfc35cb13cd756757c1e9cbeb192
Katie Mangan <katie.mangan1@gmail.com>
2019-03-26 17:25:35 -0500

    hard code 5 markers on map

--- src/components/GoogleMaps/GoogleMaps.js
+++ src/components/GoogleMaps/GoogleMaps.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
-import { GoogleApiWrapper, Map, Marker, InfoWindow } from "google-maps-react";
+import { GoogleApiWrapper, Map, Marker } from "google-maps-react";
+import './GoogleMaps.css';
+import axios from 'axios';
+import { connect } from 'react-redux';
 
-class MapContainer extends Component {
 
+class GoogleMaps extends Component {
+
+    getMyParks = () => {
+        axios.get('/myparks')
+            .then((response) => {
+                this.props.dispatch({ type: 'SET_MY_PARKS', payload: response.data })
+            }).catch(error => {
+                console.log('error in my parks client get request', error);
+            });
+    }
 
     state = {
         userLocation: {
@@ -13,6 +25,7 @@ class MapContainer extends Component {
     };
 
     componentDidMount() {
+        this.getMyParks();
         navigator.geolocation.getCurrentPosition(
             position => {
                 const { latitude, longitude } = position.coords;
@@ -49,18 +62,44 @@ class MapContainer extends Component {
         }
 
         return (
+        <div className="map">
         <Map google={google} initialCenter={userLocation} zoom={4}>
                 <Marker
                     title={'The marker`s title will appear as a tooltip.'}
-                    name={'SOMA'}
+                    name={'Grand Canyon National Park'}
                     position={{ lat: 36.17280161, lng: -112.6836024 }} />
+                    <Marker
+                    title={'The marker`s title will appear as a tooltip.'}
+                    name={'Great Smoky Mountains National Park'}
+                    position={{ lat: 35.60116374, lng: -83.50818326 }} />
+                    <Marker
+                    title={'The marker`s title will appear as a tooltip.'}
+                    name={'Grand Teton National Park'}
+                    position={{ lat: 43.81853565, lng: -110.7054666 }} />
+                    <Marker
+                    title={'The marker`s title will appear as a tooltip.'}
+                    name={'Rocky Mountain National Park'}
+                    position={{ lat: 40.3556924, lng: -105.6972879 }} />
+                    <Marker
+                    title={'The marker`s title will appear as a tooltip.'}
+                    name={'Glacier National Park'}
+                    position={{ lat: 48.68414678, lng: -113.8009306 }} />
         
         </Map>
+            </div>
         );
 
     }
 }
+const mapStateToProps = state => ({
+    //user: state.user,
+    parks: state.parks,
+    parkdisplay: state.parkdisplay,
+    editpark: state.editpark,
+});
+
+// export default withStyles(styles)(connect(mapStateToProps)(MyParks));
 
 export default GoogleApiWrapper({
     apiKey: "AIzaSyCHYi2wtrFyckBkYUz6Brsx7mB2z5khGHM"
-})(MapContainer);
+})(connect(mapStateToProps)(GoogleMaps));
